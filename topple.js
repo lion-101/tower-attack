e0cb59d0c244b147407691c1656aef42d7385124
lion-101 <65544694+lion-101@users.noreply.github.com>
2020-08-15 12:08:23 +0400

    Add files via upload

---
+++ topple.js
@@ -0,0 +1,26 @@
+class Topple {
+    constructor(x,y,width,height){
+        var options = {
+            restituition :0.4,
+            friction :0.0,
+            isStatic:true
+
+        }
+        this.body=Bodies.rectangle (x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world, this.body);
+    
+}
+display (){
+    var angle=this.body.angle;
+    //var pos=this.body.position;
+    push();
+    translate(this.body.position.x, this.body.position.y)
+    rotate(angle);
+
+    rectMode(CENTER)
+    rect(0, 0, this.width, this.height);
+    pop();
+}
+}
